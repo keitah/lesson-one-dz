3f6ed83031545c09fb4854ab9fe6a2b289955acf
iron <gpume@mail.ru>
2025-08-01 02:22:35 +0300

    more comments

--- src/main/java/abramyan/CircleRunner.java
+++ src/main/java/abramyan/CircleRunner.java
@@ -2,12 +2,12 @@ package abramyan; // Abramyan joined the game
 
 public class CircleRunner {
     public static void main(String[] args) {
-        double pi = 3.14;
-        double L = 6;
+        double pi = 3.14; // п = 3.14
+        double L = 6; // длина окружности
 
-        double D = L / pi;
-        double S = pi * D * D / 4;
+        double D = L / pi; // ищем диаметр, который равен длине окружности, делённой на п=3.14
+        double S = pi * D * D / 4; // ищем площадь круга, который равен п*D^2/4
 
-        System.out.println("Площадь круга: " + S);
+        System.out.println("Площадь круга: " + S); // выводим площадь круга
     }
 }
